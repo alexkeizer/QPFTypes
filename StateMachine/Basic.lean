12f9f41017f22c5efed3964e5f8969502e73b806
Alex Keizer <alex@keizer.dev>
2025-10-14 15:55:33 +0100

    rename StateCofix -> Coalg

--- StateMachine/Basic.lean
+++ StateMachine/Basic.lean
@@ -10,7 +10,7 @@ universe u v w
 variable {n} (F : TypeVec.{u} (n + 1) → Type u) [MvQPF F]
 
 /-- A pointed coalgebra -/
-structure StateCofix (α : TypeVec.{u} n) : Type (u+1) where
+structure Coalg (α : TypeVec.{u} n) : Type (u+1) where
   corec ::
   {σ : Type u}
   (dest' : σ → F (α ::: σ))
@@ -18,8 +18,8 @@ structure StateCofix (α : TypeVec.{u} n) : Type (u+1) where
 
 variable {F} {α : TypeVec.{u} n}
 
-namespace StateCofix
-variable {self : StateCofix F α}
+namespace Coalg
+variable {self : Coalg F α}
 
 /-! ## Basic API -/
 
@@ -33,7 +33,7 @@ so we content ourselves with having the current `dest` projection, which returns
 a state in the current coalgebra.
 -/
 
-def dest (self : StateCofix F α) : F (α ::: self.σ) := self.dest' self.s
+def dest (self : Coalg F α) : F (α ::: self.σ) := self.dest' self.s
 
 /-! ## Bisim -/
 
@@ -62,7 +62,7 @@ Given two pointed coalgebras `c` and `d`, return the union coalgebra which embed
 both coalgebras, with `c` as the designated point, together with the state of the
 union coalgebra which represents `d`.
 -/
-def union (c d : StateCofix F α) : StateCofix F α where
+def union (c d : Coalg F α) : Coalg F α where
   σ := c.σ ⊕ d.σ
   s := .inl c.s
   dest' s := match s with
@@ -70,7 +70,7 @@ def union (c d : StateCofix F α) : StateCofix F α where
           | .inr s => MvFunctor.map (TypeVec.id ::: .inr) (d.dest' s)
 
 
-def Bisim : StateCofix F α → StateCofix F α → Prop := fun c d =>
+def Bisim : Coalg F α → Coalg F α → Prop := fun c d =>
   let un := c.union d
   BisimStates (self := un) (.inl c.s) (.inr d.s)
 
@@ -78,11 +78,11 @@ infixl:60 " ~ " => Bisim
 
 section UnionLemmas
 
-theorem union_dest_eq_left (c d : StateCofix F α) (s : c.σ) :
+theorem union_dest_eq_left (c d : Coalg F α) (s : c.σ) :
     (TypeVec.id ::: Sum.inl) <$$> c.dest' s = (c.union d).dest' (.inl s) :=
   rfl
 
-theorem union_dest_eq_right (c d : StateCofix F α) (s : d.σ) :
+theorem union_dest_eq_right (c d : Coalg F α) (s : d.σ) :
     (TypeVec.id ::: Sum.inr) <$$> d.dest' s = (c.union d).dest' (.inr s) :=
   rfl
 
@@ -92,10 +92,10 @@ end UnionLemmas
 ## Conversions to/from default Cofix type
 -/
 
-def toCofix (self : StateCofix F α) : Cofix F α :=
+def toCofix (self : Coalg F α) : Cofix F α :=
   Cofix.corec self.dest' self.s
 
-def ofCofix (fix : Cofix F α) : StateCofix F α :=
+def ofCofix (fix : Cofix F α) : Coalg F α :=
   corec Cofix.dest fix
 
 @[simp]
@@ -118,7 +118,7 @@ theorem toCofix_ofCofix (fix : Cofix F α) : toCofix (ofCofix fix) = fix := by
       <;> rfl
   · rfl
 
-theorem ofCofix_toCofix (self : StateCofix F α) : ofCofix (toCofix self) ~ self := by
+theorem ofCofix_toCofix (self : Coalg F α) : ofCofix (toCofix self) ~ self := by
   simp only [toCofix, ofCofix]
   simp only [Bisim, BisimStates, union]
   use fun
@@ -142,10 +142,10 @@ theorem ofCofix_toCofix (self : StateCofix F α) : ofCofix (toCofix self) ~ self
   · intro i j
     cases i <;> rfl
 
-end StateCofix
+end Coalg
 
 variable (F) (α) in
-def StateCofixFinal := Quot (StateCofix.Bisim (F := F) (α := α))
+def StateCofixFinal := Quot (Coalg.Bisim (F := F) (α := α))
 
 namespace StateCofixFinal
 
@@ -154,7 +154,7 @@ namespace StateCofixFinal
 -/
 
 def corec {β} (f : β → F (α ::: β)) (init : β) : StateCofixFinal F α :=
-  Quot.mk _ <| StateCofix.corec f init
+  Quot.mk _ <| Coalg.corec f init
 
 /-!
 Now the big question is, how do we define `dest` for the final coalgebra here?
@@ -177,14 +177,14 @@ def toCofix (self : StateCofixFinal F α) : Cofix F α :=
     let R' : Cofix F α → Cofix F α → Prop := fun x y =>
       ∃ (s : a.σ) (t : b.σ),
           R (.inl s) (.inr t)
-          ∧ x = StateCofix.toCofix { a with s := s }
-          ∧ y = StateCofix.toCofix { b with s := t }
+          ∧ x = Coalg.toCofix { a with s := s }
+          ∧ y = Coalg.toCofix { b with s := t }
     apply MvQPF.Cofix.bisim R'
     · simp only [forall_exists_index, and_imp, R']
       rintro _ _ s t hR rfl rfl
       sorry
     · exact ⟨_, _, h_R, rfl, rfl⟩
-  Quot.lift StateCofix.toCofix h self
+  Quot.lift Coalg.toCofix h self
 
 def ofCofix (fix : Cofix F α) : StateCofixFinal F α :=
   Quot.mk _ <| .ofCofix fix
@@ -198,7 +198,7 @@ theorem ofCofix_toCofix (self : StateCofixFinal F α) : ofCofix (toCofix self) =
   simp only [ofCofix, toCofix]
   cases self using Quot.induction_on
   apply Quot.sound
-  simpa using StateCofix.ofCofix_toCofix _
+  simpa using Coalg.ofCofix_toCofix _
 
 def equivCofix : StateCofixFinal F α ≃ Cofix F α where
   toFun := toCofix
@@ -258,9 +258,9 @@ def dest (self : StateCofixShrunk F α) : F (α ::: (StateCofixShrunk F α)) :=
     show ∀ a b, a ~ b → f a = f b
     -- ^^ Non-trivial to formalize, but clearly true
     rintro a b h_bisim
-    unfold StateCofix.Bisim at h_bisim
+    unfold Coalg.Bisim at h_bisim
     rcases h_un : a.union b with ⟨un, d'⟩
-    simp only [StateCofix.dest, f]
+    simp only [Coalg.dest, f]
     -- At this point, we should rewrite `a.dest' a.s` and `b.dest' b.s` into
     -- some phrasing in terms of `union`.
     unfold corec StateCofixFinal.corec
